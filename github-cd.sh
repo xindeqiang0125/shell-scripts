0bdb1fe7219b0f64489d4f9fa093f6caf965f3b2
XINDQ <xdqnd2012@163.com>
2023-08-28 23:51:03 +0800

    1

--- github-cd.sh
+++ github-cd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 path=$1
+name=$2
 
 cd $path
 
-java -jar freedom-api.jar &
+pidof java | xargs kill
+echo "停止成功"
+
+cp __product.jar $name
+echo "拷贝执行文件成功"
+
+java -jar $name &
+echo "启动"
